917ae0465cd4b1cb8ee1aeb20ad1826513ad531c
Tim Hütz <tim@huetz.biz>
2014-01-31 15:29:42 +0100

    Added a script for cronie for starting offlineimap with gnome-keyring support

---
+++ offlineimap_cron.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+PIDS=$(pgrep awesome)
+for p in $PIDS; do
+	dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$p/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
+	user=$(grep -z USER /proc/$p/environ | sed 's/USER=//')
+	dply=$(grep -z DISPLAY /proc/$p/environ | sed 's/DISPLAY=//')
+done
+export DBUS_SESSION_BUS_ADDRESS=$dbus
+export DISPLAY=$dply
+export USER=$user
+offlineimap -l ~/offline4.log -d ALL
