cda121609dd540153e9fc8a95c816442a1ca001a
Tim Hütz <tim@huetz.biz>
2014-02-03 14:07:23 +0100

    Added code to nofiy if there is a new email (using naughty)

--- offlineimap_cron.sh
+++ offlineimap_cron.sh
@@ -8,4 +8,18 @@ done
 export DBUS_SESSION_BUS_ADDRESS=$dbus
 export DISPLAY=$dply
 export USER=$user
+idToReplace=`cat /tmp/offlineimap_cron_eventid.tmp`
+if [ -z "$idToReplace" ]; then
+	idToReplace=-1
+fi
+echo $idToReplace
+echo "DISPLAY=$dply" > /home/$user/.offlineimap.log
+echo "DBUS_SESSION_BUS_ADDRESS=$dbus" >> /home/$user/.offlineimap.log
+echo "USER=$user" >> /home/$user/.offlineimap.log
 offlineimap -l /home/$user/.offlineimap.log
+newMails=`find /home/$user/.mutt/maildir/FZJ/INBOX/new/ -type f -print | wc -l`
+if [ $newMails -gt 0 ]; then
+	lastId=`echo "local naughty = require(\"naughty\"); return naughty.notify({title = \"New email\", text = \"Your local inbox has $newMails new message(s)!\", timeout = 0, replaces_id = $idToReplace}).id" | awesome-client -`
+fi
+lastEventId=$(echo $lastId | sed 's/double\ //')
+echo $lastEventId > /tmp/offlineimap_cron_eventid.tmp
