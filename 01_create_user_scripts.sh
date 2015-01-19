8f5b0c36894b73ddeb7450dda656bd991b572b8f
Tim Hütz <tim@huetz.biz>
2015-01-19 12:57:40 +0100

    Updated the script for setting up the user configs

--- 01_create_user_scripts.sh
+++ 01_create_user_scripts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p $HOME/.config/systemd/user
 mkdir -p $HOME/.config/terminator
+
+# create the symbolic links to the provided configuration files
 ln -s $HOME/.config/my-user-configurations/shell/zshrc $HOME/.zshrc
 ln -s $HOME/.config/my-user-configurations/shell/zsh_profile $HOME/.zsh_profile
 ln -s $HOME/.config/my-user-configurations/terminator $HOME/.config/terminator/config
@@ -11,3 +13,6 @@ ln -s $HOME/.config/my-user-configurations/tmuxinator $HOME/.tmuxinator
 ln -s $HOME/.config/my-user-configurations/tmux $HOME/.tmux.conf
 ln -s $HOME/.config/my-user-configurations/mailcap $HOME/.mailcap
 ln -s $HOME/.config/my-user-configurations/uncrustify.config $HOME/.uncrustify.config
+
+# change the default shell for this user to the ZSH
+chsh -s /bin/zsh
