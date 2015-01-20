8992349fdee1c7b68966839c0f82e62214898ed0
Tim Hütz <tim@huetz.biz>
2015-01-20 15:18:31 +0100

    Added a command to create a symbolic link for the xprofile file

--- 00_setup_user_configs.sh
+++ 00_setup_user_configs.sh
@@ -10,6 +10,7 @@ ln -s $HOME/.config/my-user-configurations/shell/zsh_profile $HOME/.zsh_profile
 ln -s $HOME/.config/my-user-configurations/terminator $HOME/.config/terminator/config
 ln -s $HOME/.config/my-user-configurations/vimrc $HOME/.vimrc
 ln -s $HOME/.config/my-user-configurations/xinitrc $HOME/.xinitrc
+ln -s $HOME/.config/my-user-configurations/xprofile $HOME/.xprofile
 ln -s $HOME/.config/my-user-configurations/taskrc $HOME/.taskrc
 ln -s $HOME/.config/my-user-configurations/tmuxinator $HOME/.tmuxinator
 ln -s $HOME/.config/my-user-configurations/tmux $HOME/.tmux.conf
