24de665c0a7d57bac6c292e7a1227127ed3ba798
Tim Hütz <tim@huetz.biz>
2015-01-27 14:53:27 +0100

    Added a needed flag to the base installation

--- 01_configure_system.sh
+++ 01_configure_system.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # download and install some required packages
-yaourt -S xorg-server xorg-server-devel xorg-xinit xorg-utils xf86-input-synaptics awesome nvidia lightdm lightdm-gtk3-greeter pcmanfm numlockx networkmanager network-manager-applet terminator tmux aspell aspell-de aspell-en reflector powertop openntpd htop ttf-dejavu volumeicon conky lm_sensors smartmontools acpid acpi xterm xdg-utils ttf-google-fonts-git powerline-fonts-git ttf-ms-fonts tmuxinator google-chrome lightdm-gtk-greeter-settings pommed-light mbpfan-git --noconfirm
+yaourt -S xorg-server xorg-server-devel xorg-xinit xorg-utils xf86-input-synaptics awesome nvidia lightdm lightdm-gtk3-greeter pcmanfm numlockx networkmanager network-manager-applet terminator tmux aspell aspell-de aspell-en reflector powertop openntpd htop ttf-dejavu volumeicon conky lm_sensors smartmontools acpid acpi xterm xdg-utils ttf-google-fonts-git powerline-fonts-git ttf-ms-fonts tmuxinator google-chrome lightdm-gtk-greeter-settings pommed-light mbpfan-git --noconfirm --needed
 
 # enable some installed services
 sudo systemctl enable lightdm
