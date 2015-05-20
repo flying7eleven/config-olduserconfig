8c787da8578fa8fd07051c1f8137e6fba3db5e3e
Tim Hütz <tim@huetz.biz>
2015-05-20 13:41:25 +0200

    Remove the vim stuff (is a new repository now)

--- 00_setup_user_configs.sh
+++ 00_setup_user_configs.sh
@@ -9,8 +9,6 @@ mkdir -p $HOME/.config/mpd/
 ln -s $HOME/.config/my-user-configurations/shell/zshrc $HOME/.zshrc
 ln -s $HOME/.config/my-user-configurations/shell/zsh_profile $HOME/.zsh_profile
 ln -s $HOME/.config/my-user-configurations/terminator $HOME/.config/terminator/config
-ln -s $HOME/.config/my-user-configurations/vim $HOME/.vim
-ln -s $HOME/.config/my-user-configurations/vimrc $HOME/.vimrc
 ln -s $HOME/.config/my-user-configurations/xinitrc $HOME/.xinitrc
 ln -s $HOME/.config/my-user-configurations/xprofile $HOME/.xprofile
 ln -s $HOME/.config/my-user-configurations/taskrc $HOME/.taskrc
