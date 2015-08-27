28081e9c9c7d02c8f3f043159acf5d6211c04b89
Tim Hütz <tim@huetz.biz>
2015-08-27 13:34:45 +0200

    Removed old setting options

--- 00_setup_user_configs.sh
+++ 00_setup_user_configs.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
-mkdir -p $HOME/.config/systemd/user
-mkdir -p $HOME/.config/terminator
 mkdir -p $HOME/.local/share/applications
-mkdir -p $HOME/.cache/awesome
-mkdir -p $HOME/.config/mpd/
 
 # create the symbolic links to the provided configuration files
 ln -s $HOME/.config/my-user-configurations/shell/zshrc $HOME/.zshrc
 ln -s $HOME/.config/my-user-configurations/shell/zsh_profile $HOME/.zsh_profile
-ln -s $HOME/.config/my-user-configurations/terminator $HOME/.config/terminator/config
-ln -s $HOME/.config/my-user-configurations/xinitrc $HOME/.xinitrc
-ln -s $HOME/.config/my-user-configurations/xprofile $HOME/.xprofile
-ln -s $HOME/.config/my-user-configurations/taskrc $HOME/.taskrc
-ln -s $HOME/.config/my-user-configurations/tmuxinator $HOME/.tmuxinator
-ln -s $HOME/.config/my-user-configurations/tmux $HOME/.tmux.conf
-ln -s $HOME/.config/my-user-configurations/mailcap $HOME/.mailcap
 ln -s $HOME/.config/my-user-configurations/uncrustify.config $HOME/.uncrustify.config
-ln -s $HOME/.config/my-user-configurations/conkyrc $HOME/.conkyrc
-ln -s $HOME/.config/my-user-configurations/mpd.conf $HOME/.config/mpd/mpd.conf
-ln -s $HOME/.config/my-user-configurations/mairixrc $HOME/.mairixrc
-ln -s $HOME/.config/my-user-configurations/muttrc $HOME/.muttrc
 
 # change the default shell for this user to the ZSH
 chsh -s /bin/zsh
-
-# create a symbolic link for pommed
-echo "Creating symbolic link for pommed.conf..."
-sudo sh -c "ln -s $HOME/.config/my-user-configurations/pommed.conf /etc/pommed.conf"
 
 # set some default mime handlers
 xdg-mime default google-chrome.desktop x-scheme-handler/http
 xdg-mime default google-chrome.desktop x-scheme-handler/https
 xdg-mime default pcmanfm.desktop inode/directory
 xdg-mime default pcmanfm.desktop inode/directory
-#xdg-mime default thunderbird.desktop x-scheme-handler/mailto
-#xdg-mime default thunderbird.desktop message/rfc822 
