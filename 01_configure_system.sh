7a77681922ab5c54d4dc95f8697813c36cd74a39
Tim Hütz <tim@huetz.biz>
2015-08-27 13:35:18 +0200

    Removed some packages which are not required

--- 01_configure_system.sh
+++ 01_configure_system.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # download and install some required packages
-yaourt -S xorg-server xorg-server-devel xorg-xinit xorg-utils xf86-input-synaptics awesome nvidia lightdm lightdm-gtk-greeter pcmanfm numlockx networkmanager network-manager-applet terminator tmux aspell aspell-de aspell-en reflector powertop openntpd htop ttf-dejavu volumeicon conky lm_sensors smartmontools acpid acpi xterm xdg-utils ttf-google-fonts-git powerline-fonts-git ttf-ms-fonts tmuxinator google-chrome lightdm-gtk-greeter-settings pommed-light mbpfan-git gksu mpd ncmpcpp --noconfirm --needed
+yaourt -S xorg-server xorg-server-devel xorg-utils nvidia lightdm lightdm-gtk-greeter pcmanfm numlockx terminator tmux aspell aspell-de aspell-en reflector powertop openntpd htop ttf-dejavu lm_sensors smartmontools acpid acpi xterm xdg-utils ttf-google-fonts-git powerline-fonts-git ttf-ms-fonts tmuxinator google-chrome lightdm-gtk-greeter-settings gksu ncmpcpp --noconfirm --needed
 
 # enable some installed services
 sudo systemctl enable lightdm
 sudo systemctl enable openntpd
-sudo systemctl enable pommed
-sudo systemctl enable mbpfan
 sudo systemctl enable acpid
-sudo systemctl enable NetworkManager
 
 # generate the basic nvidia configuration file for X
 sudo Xorg -configure
